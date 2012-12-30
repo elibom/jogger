2228109c66dce11bfdd84c960f6910f8484522e0
German Escobar <german.escobarc@gmail.com>
2012-12-30 09:47:07 -0500

    Create tests for JoggerServer

--- jogger-core/src/test/java/org/jogger/JoggerServerTest.java
+++ jogger-core/src/test/java/org/jogger/JoggerServerTest.java
@@ -2,6 +2,7 @@ package org.jogger;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
+import org.jogger.http.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,10 +13,54 @@ public class JoggerServerTest {
 		JoggerServer joggerServer = new JoggerServer(new Jogger());
 		joggerServer.listen(27773);
 
-		HttpResponse response = Request.Get("http://localhost:27773/").execute().returnResponse();
-		Assert.assertEquals(response.getStatusLine().getStatusCode(), 404);
+		try {
+			HttpResponse response = Request.Get("http://localhost:27773/").execute().returnResponse();
+			Assert.assertEquals(response.getStatusLine().getStatusCode(), 404);
+		} finally {
+			joggerServer.stop();
+		}
+	}
+	
+	@Test
+	public void shouldExecuteRoute() throws Exception {
+		Jogger app = new Jogger();
+		app.get("/", new RouteHandler() {
+			@Override
+			public void handle(org.jogger.http.Request request, Response response) {
+				
+			}
+		});
 		
-		joggerServer.stop();
+		JoggerServer joggerServer = new JoggerServer(app);
+		joggerServer.listen(27773);
+
+		try {
+			HttpResponse response = Request.Get("http://localhost:27773/").execute().returnResponse();
+			Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+		} finally {
+			joggerServer.stop();
+		}
+	}
+	
+	@Test
+	public void shouldHandleException() throws Exception {
+		Jogger app = new Jogger();
+		app.get("/", new RouteHandler() {
+			@Override
+			public void handle(org.jogger.http.Request request, Response response) {
+				throw new RuntimeException();
+			}
+		});
+		
+		JoggerServer joggerServer = new JoggerServer(app);
+		joggerServer.listen(27773);
+
+		try {
+			HttpResponse response = Request.Get("http://localhost:27773/").execute().returnResponse();
+			Assert.assertEquals(response.getStatusLine().getStatusCode(), 500);
+		} finally {
+			joggerServer.stop();
+		}
 	}
 	
 }
