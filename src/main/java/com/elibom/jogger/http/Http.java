04d39a69a07f7be300e3b3f71140b01fe2dd6eda
Selim Dincer <wowselim@live.de>
2014-09-13 03:06:30 -0700

    fix typo

--- src/main/java/com/elibom/jogger/http/Http.java
+++ src/main/java/com/elibom/jogger/http/Http.java
@@ -16,7 +16,7 @@ public class Http {
 		public static final String AUTHORIZATION = "Authorization";
 		public static final String CACHE_CONTROL = "Cache-Control";
 		public static final String CONNECTION = "Connection";
-		public static final String CONTENT_TYPE = "Conten-Type";
+		public static final String CONTENT_TYPE = "Content-Type";
 		public static final String CONTENT_LENGTH = "Content-Length";
 		public static final String CONTENT_MD5 = "Content-MD5";
 		public static final String DATE = "Date";
