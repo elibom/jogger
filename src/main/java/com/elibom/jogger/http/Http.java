1639c29af9aa768b37d06beffe36d6e0a5859e88
Selim Dincer <wowselim@live.de>
2014-09-19 15:29:28 -0700

    fix one more potential typo

--- src/main/java/com/elibom/jogger/http/Http.java
+++ src/main/java/com/elibom/jogger/http/Http.java
@@ -30,7 +30,7 @@ public class Http {
 		public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
 		public static final String APPLICATION_JSON = "application/json";
 		public static final String APPLICATION_XML = "application/xml";
-		public static final String TEXT_HTML = "text/xml";
+		public static final String TEXT_HTML = "text/html";
 		public static final String MULTIPART_FORM_DATA = "multipart/form-data";
 	}
 }
